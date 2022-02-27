7cccb5ccaeeb15f3099b8f9d1683fc1b5e149b65
aidanweltner <aidan.weltner@gmail.com>
2022-02-27 15:51:12 -0600

    Added coffee TW color & updated header styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
     'hamburgers', 'is-active',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        coffee: {
+          100: '#fcfbfa',
+          200: '#f7f5f2',
+          300: '#f0ebe4',
+          400: '#e0dace',
+          500: '#bfb4a3',
+          600: '#968a75',
+          700: '#695f4d',
+          800: '#474030',
+          900: '#2b2718',
+        }
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
