c9a768b352d0a454504853552d553690f8d018bb
aidanweltner <aidan.weltner@gmail.com>
2022-12-02 18:07:34 -0600

    Downgraded gallery component to Vue2

--- resources/js/components/Gallery.vue
+++ resources/js/components/Gallery.vue
@@ -1,29 +1,32 @@
 <template>
-  <ul class="container mx-auto md:px-8 lg:px-16 xl:px-24 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-2">
-    <li v-for="(img, index) in imgs" :key="index" class="w-full h-full relative">
-      <div class="group relative w-full h-full" @click="() => showImg(index)">
-        <figure class="relative w-full h-24 xl:h-42">
-          <img :src="img.src" :alt="img.alt" class="w-full h-full object-cover"/>
-          <div class="absolute inset-0 w-full h-full group-hover:opacity-100 opacity-0 transistion-opacity ease-in-out delay-100 bg-gradient-to-t from-stone-800/60 to-transparent"></div>
-          <div class="absolute right-2 bottom-2 transition-all delay-200 ease-in-out group-hover:opacity-100 opacity-0 scale-75 group-hover:scale-100">
-            <ArrowsPointingOutIcon class="h-4 w-4 text-coffee-100" />
-          </div>
-        </figure>
-      </div>
-    </li>
-  </ul>
-  <vue-easy-lightbox
-    :visible="visibleRef"
-    :imgs="imgs"
-    :index="indexRef"
-    @hide="onHide"
-    loop="true"
-    scrollDisabled="true"
-  ></vue-easy-lightbox>
+  <div class="container mx-auto md:px-8 lg:px-16 xl:px-24">
+    <ul class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-2">
+      <li v-for="(img, index) in imgs" :key="index" class="w-full h-full relative">
+        <div class="group relative w-full h-full" @click="() => showImg(index)">
+          <figure class="relative w-full h-24 xl:h-42">
+            <img :src="img.src" :alt="img.alt" class="w-full h-full object-cover"/>
+            <div class="absolute inset-0 w-full h-full group-hover:opacity-100 opacity-0 transistion-opacity ease-in-out delay-100 bg-gradient-to-t from-stone-800/60 to-transparent"></div>
+            <div class="absolute right-2 bottom-2 transition-all delay-200 ease-in-out group-hover:opacity-100 opacity-0 scale-75 group-hover:scale-100">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4 text-coffee-100">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+              </svg>
+            </div>
+          </figure>
+        </div>
+      </li>
+    </ul>
+    <vue-easy-lightbox
+      :visible="visibleRef"
+      :imgs="imgs"
+      :index="indexRef"
+      @hide="onHide"
+      loop="true"
+      scrollDisabled="true"
+    ></vue-easy-lightbox>
+  </div>
 </template>
 
 <script>
-import {ArrowsPointingOutIcon} from '@heroicons/vue/24/outline'
 import VueEasyLightbox from 'vue-easy-lightbox'
 import { ref } from 'vue'
 
@@ -48,7 +51,6 @@ export default {
     }
   },
   components: {
-    ArrowsPointingOutIcon,
     VueEasyLightbox
   },
   props: {
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import NavBar from './components/NavBar.vue'
-/* import Gallery from './components/Gallery' */
+import Gallery from './components/Gallery'
 
 new Vue({
   el: '#app',
   components: {
     NavBar,
-    /* Gallery, */
+    Gallery,
   }
 })
